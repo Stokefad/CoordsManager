479b948fc9c4c7907b540c3efe384ddbe09ad134
Igor-Macbook Pro <igor.naum.2013@yandex.ru>
2019-01-04 02:12:24 +0300

    The app is done

--- CoordsManager/Controller/AttendanceViewController.swift
+++ CoordsManager/Controller/AttendanceViewController.swift
@@ -7,23 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class AttendanceViewController : UIViewController {
     
-
-    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var datePicker: UIView!
+    @IBOutlet weak var pickedDate: UIDatePicker!
     
+    var note : AttendanceNote = AttendanceNote()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let date = datePicker.calendar.date(byAdding: <#T##DateComponents#>, to: <#T##Date#>)
+    }
+
+    
+    @IBAction func findButtonPressed(_ sender: UIButton) {
+        let date = pickedDate.date
         
+        findDay(date: date)
     }
     
-    @IBAction func datePicked(_ sender: UIDatePicker) {
-        
-    }
     
+    private func findDay(date : Date) {
+        let request : NSFetchRequest<AttendanceNote> = AttendanceNote.fetchRequest()
+        request.predicate = NSPredicate(format: "date = %@", "\(date)")
+        
+        do {
+            note = try context.fetch(request)[0]
+        }
+        catch {
+            print("Error with retrieving occured \(error)")
+        }
+    }
     
 }
